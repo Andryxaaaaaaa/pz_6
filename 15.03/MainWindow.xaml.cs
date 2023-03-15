53d51dbbaacee0eae74a407e506fd28e522980d3
Андрей Полянский <andreudasug@gmail.com>
2023-03-15 15:28:05 +0500

    1

--- 15.03/MainWindow.xaml.cs
+++ 15.03/MainWindow.xaml.cs
@@ -28,20 +28,15 @@ namespace _15._03
             
             
         }
-        static void Method1()
-        {
-            
-            
-
-        }
+        
+        
         
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Stopwatch timer = Stopwatch.StartNew();
+            
             Console.WriteLine("Запуск методов асинхронно в потоке");
-            Task task1 = new Task(Method1);
-            task1.Start();
+            
             
             for (int i = 0; i < 100;)
             {
